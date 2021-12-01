c6f46b534dfa5f71d642fc90a3c5fc01d21a1f06
Motin <motin@motin.eu>
2021-12-01 12:30:46 +0200

    Typo

--- tests/test_oefdb.py
+++ tests/test_oefdb.py
@@ -1,4 +1,4 @@
 def test_oefdb_import_statement() -> None:
     import oefdb  # noqa: F401
 
-    assert "Empty assert to ensure pytest passes as long as the above imports succeeds."
+    assert "Empty assert to ensure pytest passes as long as the above import succeeds."
