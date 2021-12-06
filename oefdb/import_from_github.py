a3b87a63bd34539f24d057366f7ac3b7792e6fc9
Motin <motin@motin.eu>
2021-12-06 09:47:14 +0200

    Actually import the PR's specific data

--- oefdb/import_from_github.py
+++ oefdb/import_from_github.py
@@ -4,6 +4,8 @@ from typing import Optional
 from github import Repository
 from pandas import DataFrame, read_csv
 
+oefdb_csv_filename = "OpenEmissionFactorsDB.csv"
+
 
 def import_from_github(pr: Optional[int] = None) -> DataFrame:
     if pr:
@@ -23,7 +25,7 @@ def get_oefdb_repo(
 def import_from_github_pr(number: int) -> DataFrame:
     repo = get_oefdb_repo()
     pr = repo.get_pull(number)
-    return read_csv(get_oefdb_csv_contents(pr.head.repo))
+    return read_csv(get_oefdb_csv_contents(pr.head.repo, pr.head.ref))
 
 
 def import_from_github_main_branch() -> DataFrame:
@@ -31,6 +33,9 @@ def import_from_github_main_branch() -> DataFrame:
     return read_csv(get_oefdb_csv_contents(repo))
 
 
-def get_oefdb_csv_contents(repo: Repository) -> BytesIO:
-    contents = repo.get_contents("OpenEmissionFactorsDB.csv")
+def get_oefdb_csv_contents(repo: Repository, ref: Optional[str] = None) -> BytesIO:
+    if ref:
+        contents = repo.get_contents(oefdb_csv_filename, ref)
+    else:
+        contents = repo.get_contents(oefdb_csv_filename)
     return BytesIO(contents.decoded_content)
