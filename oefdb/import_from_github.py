0a9cb29b3d151eedd055e0e7a36ed5f2c6d6983d
Motin <motin@motin.eu>
2021-12-17 10:37:47 +0200

    Use motin's fork of oefdb repo in tests, so that they are independent of main repo PR activity

--- oefdb/import_from_github.py
+++ oefdb/import_from_github.py
@@ -7,29 +7,29 @@ from pandas import DataFrame, read_csv
 oefdb_csv_filename = "OpenEmissionFactorsDB.csv"
 
 
-def import_from_github(pr: Optional[int] = None) -> DataFrame:
+def import_from_github(
+    pr: Optional[int] = None, repo_reference: str = "climatiq/Open-Emission-Factors-DB"
+) -> DataFrame:
     if pr:
-        return import_from_github_pr(pr)
-    return import_from_github_main_branch()
+        return import_from_github_pr(pr, repo_reference=repo_reference)
+    return import_from_github_main_branch(repo_reference=repo_reference)
 
 
-def get_oefdb_repo(
-    full_name_or_id: str = "climatiq/Open-Emission-Factors-DB",
-) -> Repository:
+def get_oefdb_repo(repo_reference: str) -> Repository:
     from github import Github
 
     g = Github()
-    return g.get_repo(full_name_or_id)
+    return g.get_repo(repo_reference)
 
 
-def import_from_github_pr(number: int) -> DataFrame:
-    repo = get_oefdb_repo()
+def import_from_github_pr(number: int, repo_reference: str) -> DataFrame:
+    repo = get_oefdb_repo(repo_reference)
     pr = repo.get_pull(number)
     return read_csv(get_oefdb_csv_contents(pr.head.repo, pr.head.ref))
 
 
-def import_from_github_main_branch() -> DataFrame:
-    repo = get_oefdb_repo()
+def import_from_github_main_branch(repo_reference: str) -> DataFrame:
+    repo = get_oefdb_repo(repo_reference)
     return read_csv(get_oefdb_csv_contents(repo))
 
 
