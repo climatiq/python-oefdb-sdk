31453dd87f02bc7a21de9799f6ff9b145cdeffab
Motin <motin@motin.eu>
2022-01-19 12:27:12 +0200

    Add test coverage for the existing validators

---
+++ oefdb/validators/__init__.py
@@ -0,0 +1,7 @@
+# Export the default functions from each file,
+# allowing the functions to be organized in one file each
+from .check_for_duplicates import check_for_duplicates  # noqa: F401
+from .check_ids_for_unsupported_characters import (  # noqa: F401
+    check_ids_for_unsupported_characters,
+)
+from .check_oefdb_structure import check_oefdb_structure  # noqa: F401
---
+++ tests/validators/test_check_for_duplicates.py
@@ -0,0 +1,25 @@
+from tests.utils import load_oefdb_fixture
+
+
+def test_check_for_duplicates():
+    from oefdb.validators import check_for_duplicates
+
+    oefdb_df_without_duplicates = load_oefdb_fixture("oefdb_df_without_duplicates")
+    validation_result, validation_messages = check_for_duplicates(
+        oefdb_df_without_duplicates
+    )
+    assert validation_result is True
+
+    oefdb_df_with_a_duplicate_row = load_oefdb_fixture("oefdb_df_with_a_duplicate_row")
+    validation_result, validation_messages = check_for_duplicates(
+        oefdb_df_with_a_duplicate_row
+    )
+    assert validation_result is False
+
+    oefdb_df_with_a_compound_key_duplicate = load_oefdb_fixture(
+        "oefdb_df_with_a_compound_key_duplicate"
+    )
+    validation_result, validation_messages = check_for_duplicates(
+        oefdb_df_with_a_compound_key_duplicate
+    )
+    assert validation_result is False
