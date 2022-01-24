e14e2ada31caac2f75024a4724cad1905f0548ba
Motin <motin@motin.eu>
2022-01-24 21:32:02 +0200

    Use Climatiq's oefdb dev repo instead of motin's fork in testing

--- tests/test_import_from_github.py
+++ tests/test_import_from_github.py
@@ -1,8 +1,10 @@
 def test_import_from_github() -> None:
     import oefdb
 
-    oefdb.import_from_github(repo_reference="motin/Open-Emission-Factors-DB")
-    oefdb.import_from_github(pr=1, repo_reference="motin/Open-Emission-Factors-DB")
+    oefdb.import_from_github(repo_reference="climatiq/Open-Emission-Factors-DB-Dev")
+    oefdb.import_from_github(
+        pr=1, repo_reference="climatiq/Open-Emission-Factors-DB-Dev"
+    )
 
     assert (
         "Empty assert to ensure pytest passes as long as the above commands succeeds."
