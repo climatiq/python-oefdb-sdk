442d45e2dde198ffc09e91c2cd3325c7347454a9
Motin <motin@motin.eu>
2022-01-26 12:09:56 +0200

    Use from_oefdb_csv also in load_oefdb_fixture test util

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,11 @@
 from os.path import dirname, join, realpath
 
-import pandas as pd
 from pandas import DataFrame
 
 fixtures_dir_path = join(dirname(realpath(__file__)), "fixtures")
 
 
 def load_oefdb_fixture(ref: str) -> DataFrame:
-    return pd.read_csv(join(fixtures_dir_path, f"{ref}.csv"))
+    from oefdb import from_oefdb_csv
+
+    return from_oefdb_csv(join(fixtures_dir_path, f"{ref}.csv"))
