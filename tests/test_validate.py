a66370695a8b43d36565f1257946d456169be9d9
Motin <motin@motin.eu>
2022-01-26 10:12:34 +0200

    Renamed a variable

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -6,6 +6,6 @@ def test_validate() -> None:
 
     valid_oefdb_df = load_oefdb_fixture("valid_oefdb_df")
 
-    validation_result, validation_messages = oefdb.validate(valid_oefdb_df)
+    validation_result, results_from_validators = oefdb.validate(valid_oefdb_df)
 
     assert validation_result is True
