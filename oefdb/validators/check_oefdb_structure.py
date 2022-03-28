1a5da41b0439b0c4ff3d558b6ed122db0aa53345
Anastasia Lobanova <32800112+AnastasiaLobanova@users.noreply.github.com>
2022-03-28 12:42:31 +0200

    Checker update after adding new column in the db (#22)
    * checker update after adding new column in the db
    
    * small error

--- oefdb/validators/check_oefdb_structure.py
+++ oefdb/validators/check_oefdb_structure.py
@@ -46,6 +46,7 @@ def check_oefdb_structure(df: DataFrame) -> validator_result_type:
             "years_valid",
             "years_calculated_from",
             "region",
+            "data_quality",
             "date_accessed",
             "description",
             "source_link",
