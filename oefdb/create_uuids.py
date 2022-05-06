3e9ec3819268300cce95856aa17a67531280a695
Motin <motin@motin.eu>
2022-05-06 11:24:57 +0300

    Address linting issues

--- oefdb/create_uuids.py
+++ oefdb/create_uuids.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
 import uuid
-import numpy as np
-import pandas as pd
 
-def assign_uuids_to_new_entries(df) -> DataFrame:
-    
+from pandas import DataFrame, isna
+
+
+def assign_uuids_to_new_entries(df: DataFrame) -> DataFrame:
+
     for i in range(len(df)):
-        if pd.isna(df.loc[i,"UUID"]):
-            df.loc[i,"UUID"]=uuid.uuid4()
+        if isna(df.loc[i, "UUID"]):
+            df.loc[i, "UUID"] = uuid.uuid4()
     return df
