bbe25d316c860839c2be4bb0baca0309c810804d
Motin <motin@motin.eu>
2022-05-06 09:49:32 +0300

    Upgrade to pandas >=1.4.2

--- oefdb/util/from_oefdb_csv.py
+++ oefdb/util/from_oefdb_csv.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 from pandas import DataFrame, read_csv
-from pandas._typing import FilePathOrBuffer
+from pandas._typing import FilePath, ReadCsvBuffer
 
 
-def from_oefdb_csv(filepath_or_buffer: FilePathOrBuffer) -> DataFrame:
+def from_oefdb_csv(
+    filepath_or_buffer: FilePath | ReadCsvBuffer[bytes] | ReadCsvBuffer[str],
+) -> DataFrame:
     return read_csv(filepath_or_buffer=filepath_or_buffer)
